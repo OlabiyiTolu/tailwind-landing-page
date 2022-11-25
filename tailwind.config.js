68b9336e898ef73970b6f1692d0f06fcbe71ee7a
OlabiyiTolu <olabiyitolu@gmail.com>
2022-11-25 12:11:06 +0100

    commits

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html, ./css/*.html'],
+  content: ['./*.html, ./css/*.css'],
   theme: {
     screens: {
       sm: '480px',
